44e65125f61b7a909dcb1e6f2bda93e18a0752e2
Bonnie Magland <bonnie.magland@gmail.com>
2020-08-20 20:34:16 -0400

    done

---
+++ run_this.py
@@ -0,0 +1,20 @@
+"""I think I need a shabang here or something
+but I forget what's up with that."""
+
+
+
+if __name__ == "__main__":
+    user_input = input("Enter your favorie number: ")
+    if user_input == '2':
+        valid_answer = False
+        while valid_answer == False:
+            answer = input("Are you my father?")
+            if answer == 'yes':
+                print("Luke, I am your father!")
+                valid_answer = True
+            elif answer == 'no':
+                print("Oh, well that\'s my dad\'s favorite number too.")
+                valid_answer = True
+            else:
+                print("Please enter yes or no")
+    
