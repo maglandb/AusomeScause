504812f7169ae297490b06877c5275b508ca8c85
Jeremy Magland <jmagland@flatironinstitute.org>
2020-08-20 20:53:46 -0400

    misc updates

---
+++ README.md
@@ -0,0 +1,2 @@
+Github will only display README, README.txt, or README.md on the front page
+(not READ_ME)
--- run_this.py
+++ run_this.py
@@ -1,14 +1,12 @@
-"""I think I need a shabang here or something
-but I forget what's up with that."""
-
-
+#!/usr/bin/env python
 
 if __name__ == "__main__":
     user_input = input("Enter your favorie number: ")
     if user_input == '2':
+        # hmmm, I'm not sure what's up with this valid_answer variable. Do you ever use it for anything?
         valid_answer = False
         while valid_answer == False:
-            answer = input("Are you my father? ")
+            answer = input("Are you my father?")
             if answer == 'yes':
                 print("Luke, I am your father!")
                 valid_answer = True
@@ -18,5 +16,5 @@ if __name__ == "__main__":
             else:
                 print("Please enter yes or no")
     else:
-        print("Lame. Goodbye.")
+        print("You are not my father.")
     
