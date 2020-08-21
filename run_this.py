3d018c2ecc1be97689888a61fc28c460d7c76ab0
william magland <williammagland@pop-os.localdomain>
2020-08-20 20:54:00 -0400

    onemorechance

--- run_this.py
+++ run_this.py
@@ -2,21 +2,23 @@
 but I forget what's up with that."""
 
 
-
-if __name__ == "__main__":
-    user_input = input("Enter your favorie number: ")
-    if user_input == '2':
-        valid_answer = False
-        while valid_answer == False:
-            answer = input("Are you my father? ")
-            if answer == 'yes':
-                print("Luke, I am your father!")
-                valid_answer = True
-            elif answer == 'no':
-                print("Oh, well that\'s my dad\'s favorite number too.")
-                valid_answer = True
-            else:
-                print("Please enter yes or no")
-    else:
-        print("Lame. Goodbye.")
-    
+def start():
+    if __name__ == "__main__":
+        user_input = input("Enter your favorie number: ")
+        if user_input == '2':
+            valid_answer = False
+            while valid_answer == False:
+                answer = input("Are you my father? ")
+                if answer == 'yes':
+                    print("Luke, I am your father!")
+                    valid_answer = True
+                elif answer == 'no':
+                    print("Oh, well that\'s my dad\'s favorite number too.")
+                    valid_answer = True
+                else:
+                    print("Please enter yes or no")
+        else:
+            print("Lame. Goodbye.")
+            print("alright, Ill give you one more chance.")
+            start()
+start()
